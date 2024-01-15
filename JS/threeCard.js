0f840275b0f26a07cf65bcba3ef69790e2c71142
okanberhoglu <okanberh@gmail.com>
2024-01-15 16:59:07 +0300

    3D models added and design updated.

---
+++ JS/threeCard.js
@@ -0,0 +1,49 @@
+import * as THREE from "three";
+import { OrbitControls } from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
+
+const width = document.getElementById("contactCard").clientWidth;
+const height = document.getElementById("contactCard").clientHeight; 
+
+const scene = new THREE.Scene();
+
+
+const camera = new THREE.PerspectiveCamera(35, height / width, 0.1, 2000);
+const renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
+renderer.setSize(width, height);
+renderer.setClearColor( 0x000000, 0 );
+document.getElementById("contactCard").appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(13, 10, 0.5);
+
+const texture = new THREE.TextureLoader().load("../assets/card.jpg");
+
+const material = [
+    new THREE.MeshLambertMaterial( {color: 0x365486 }),
+    new THREE.MeshLambertMaterial( {color: 0x596FB7 }),
+    new THREE.MeshLambertMaterial( {color: 0x596FB7 }),
+    new THREE.MeshLambertMaterial( {color: 0x365486 }),
+    new THREE.MeshLambertMaterial( {color: 0x11235A }),
+    new THREE.MeshBasicMaterial( {map: texture, transparent: true }),
+]
+const card = new THREE.Mesh(geometry, material);
+card.rotateY(4)
+scene.add(card)
+
+const light = new THREE.AmbientLight({color: 'white'});
+light.position.set(1, 1, 1);
+scene.add(light);
+
+camera.position.z = 0;
+camera.position.y = 3;
+camera.position.x = 27;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableZoom = false;
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    card.rotateY(0.01);
+    renderer.render(scene, camera);
+  }
+  animate();
---
+++ JS/threeMain.js
@@ -0,0 +1,39 @@
+import * as THREE from "three";
+import { OrbitControls } from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
+
+const width = document.getElementById("aboutThree").parentElement.clientWidth/2;
+const height = document.getElementById("aboutThree").parentElement.clientHeight;
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0x0f1729 );
+
+const camera = new THREE.PerspectiveCamera(45, height / width, 0.1, 2000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+document.getElementById("aboutThree").appendChild(renderer.domElement);
+
+const geometry = new THREE.TorusGeometry(20,4,16,100,10);
+const material = new THREE.MeshPhongMaterial();
+const model = new THREE.Mesh(geometry, material);
+scene.add(model);
+
+const light = new THREE.AmbientLight({color: "white"});
+light.position.set(5, 20, 0);
+scene.add(light);
+camera.position.z = 50;
+camera.position.y = 0;
+camera.position.x = 50;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableZoom = false;
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  model.rotateX(0.02);
+  model.rotateY(-0.02);
+  model.rotateZ(0.02);
+  renderer.render(scene, camera);
+}
+animate();
